a3e345e471db24c998accc2a867e1482ec6a48c3
Henry <henrymendoza2098@gmail.com>
2026-04-22 16:39:15 -0400

    urls

--- sistema_iujo/urls.py
+++ sistema_iujo/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from inventario import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Sistema de Autenticación integrado 
+    path('accounts/', include('django.contrib.auth.urls')),  
+# Rutas de la App 
+    path('', views.lista_productos, name='lista_productos'), 
+    path('nuevo/', views.crear_producto, name='crear_producto'),
 ]
