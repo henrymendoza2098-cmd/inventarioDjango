73e2840c04f10db3196b6470884d9ce36a79b106
Henry <henrymendoza2098@gmail.com>
2026-04-22 17:06:27 -0400

    final

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Producto
+from .models import Categoria
 
-# Register your models here.
+admin.site.register(Producto)
+admin.site.register(Categoria)
